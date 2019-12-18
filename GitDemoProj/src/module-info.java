99af162400ff2e977e106a22197722de1d05f5f3
surilinx <elchuri.suresh1@gmail.com>
2019-12-18 17:36:22 -0500

    demoproj commit

---
+++ GitDemoProj/src/module-info.java
@@ -0,0 +1,11 @@
+public class Test {
+	public static void main(String[] args) {
+		String hungry = "yes";
+		
+		if (hungry == "yes")
+			System.out.println("eat sommething...");
+		else {
+			System.out.println("do some thing work u have..")
+		}
+	}
+}
---
+++ GitDemoProj/src/samplepro/test.java
@@ -0,0 +1,13 @@
+package samplepro;
+
+public class test {	
+			public static void main(String[] args) {
+				String hungry = "yes";
+				
+				if (hungry == "yes")
+					System.out.println("eat sommething...");
+				else {
+					System.out.println("do some thing work u have..");
+				}
+			}
+		}
